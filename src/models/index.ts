a8c6d5dd5b83fd5eec18338de14ad979d17666c8
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2022-12-13 18:33:00 +0400

    remove models

--- src/models/index.ts
+++
@@ -1,9 +0,0 @@
-import User from "./user.model";
-import Token from "./token.model";
-import Guild from "./Guild";
-
-export {
-    User,
-    Token,
-    Guild
-}
--- src/models/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-import toJSON from "./toJSON.plugin";
-
-export {
-    toJSON
-};
