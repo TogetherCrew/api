4d04de13c259d6b4f778d3ad77ea051f22fe395c
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-07 12:38:46 +0400

    fix swagger

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import docsRoute from './docs.route';
 import authRoute from './auth.route';
-import config from "../../config";
+
 const router = express.Router();
 
 const defaultRoutes = [
@@ -15,22 +15,10 @@ const defaultRoutes = [
     }
 ];
 
-const devRoutes = [
-    // routes available only in development mode
-    {
-        path: '/docs',
-        route: docsRoute,
-    },
-];
+
 
 defaultRoutes.forEach((route) => {
     router.use(route.path, route.route);
 });
-
-if (config.env === 'development') {
-    devRoutes.forEach((route) => {
-        router.use(route.path, route.route);
-    });
-}
 
 export default router;
