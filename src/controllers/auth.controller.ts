cb5cedd7f959832d9afaa05410e34afedda882a2
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-11 11:06:01 +0400

    work on auth login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,21 +4,23 @@ import config from '../config';
 import { scopes, permissions } from '../config/dicord'
 import { userService, authService, tokenService, guildService } from '../services';
 import { IDiscordUser, IDiscordOathBotCallback } from 'tc-dbcomm';
-import { catchAsync, ApiError } from "../utils";
+import { catchAsync } from "../utils";
+import querystring from 'querystring';
 
 const login = catchAsync(async function (req: Request, res: Response) {
     res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.discord.clientId}&redirect_uri=${config.discord.callbackURI}&response_type=code&scope=${scopes.bot}&permissions=${permissions.ViewChannels}`);
 });
 
-const callback = catchAsync(async function (req: Request, res: Response, next: NextFunction) {
+const callback = catchAsync(async function (req: Request, res: Response) {
     const code = req.query.code as string;
     if (!code) {
-        throw new ApiError(httpStatus.FORBIDDEN, req.query.error_description as string);
+        throw new Error();
     }
     try {
         const discordOathCallback: IDiscordOathBotCallback = await authService.exchangeCode(code);
         const discordUser: IDiscordUser = await userService.getUserFromDiscordAPI(discordOathCallback.access_token);
         let user = await userService.getUserByDiscordId(discordUser.id);
+        console.log(1)
         if (!user) {
             user = await userService.createUser(discordUser);
         }
@@ -26,13 +28,25 @@ const callback = catchAsync(async function (req: Request, res: Response, next: N
         if (!guild) {
             guild = await guildService.createGuild(discordOathCallback.guild, user.discordId);
         }
+        console.log(3)
         tokenService.saveDiscordAuth(user.discordId, discordOathCallback);
-        const tokens = await tokenService.generateAuthTokens(user.discordId);
-        res.status(httpStatus.OK).send({ user, guild, tokens });
+        console.log(2)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const tokens: any = await tokenService.generateAuthTokens(user.discordId);
+        const query = querystring.stringify({
+            "isSuccessful": true,
+            "accessToken": tokens.access,
+            "refreshToken": tokens.refresh,
+            "guildId": guild.guildId,
+        });
+        res.redirect('http://localhost:3000/login?' + query);
     } catch (err) {
-        next(err)
+        console.log(err)
+        const query = querystring.stringify({
+            "isSuccessful": false
+        });
+        res.redirect('http://localhost:3000/login?' + query);
     }
-
 });
 
 const logout = catchAsync(async function (req: Request, res: Response) {
