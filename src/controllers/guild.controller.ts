d3881901df108adfdfc4c2b9a500763f0c479058
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-14 11:40:09 +0400

    sort channels

--- src/controllers/guild.controller.ts
+++ src/controllers/guild.controller.ts
@@ -9,7 +9,22 @@ const getGuildChannels = catchAsync(async function (req: IAuthRequest, res: Resp
         throw new ApiError(httpStatus.BAD_REQUEST, 'Please add the RnDAO bot to your server');
     }
     const channels = await guildService.getGuildChannels(req.params.guildId);
-    res.send(channels)
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const sortedChanneles: Array<any> = [];
+    for (let i = 0; i < channels.length; i++) {
+        if (channels[i].parent_id === null) {
+            sortedChanneles.push({ id: channels[i].id, title: channels[i].name, subChannels: [] })
+        }
+    }
+    for (let i = 0; i < sortedChanneles.length; i++) {
+        for (let j = 0; j < channels.length; j++) {
+            if (sortedChanneles[i].id === channels[j].parent_id) {
+                sortedChanneles[i].subChannels.push(channels[j]);
+            }
+        }
+    }
+    res.send(sortedChanneles)
 });
 
 const updateGuild = catchAsync(async function (req: IAuthRequest, res: Response) {
