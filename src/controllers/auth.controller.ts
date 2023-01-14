152736fa9f6e14ad29374720a8597cb7cac48d66
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-14 11:53:59 +0400

    log tokens

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -39,6 +39,7 @@ const callback = catchAsync(async function (req: Request, res: Response) {
             "guildId": guild.guildId,
             "guildName": guild.name
         });
+        console.log(tokens.access.token, tokens.refresh.token)
         res.redirect('http://localhost:3000/login?' + query);
     } catch (err) {
         const query = querystring.stringify({
