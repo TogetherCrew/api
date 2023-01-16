209336f46e9b2f954118c74608f0da1d2443a749
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-16 10:36:25 +0400

    refacotr store channles func

--- src/services/channel.service.ts
+++ src/services/channel.service.ts
@@ -11,9 +11,15 @@ async function sortChannels(channels: Array<any>) {
     for (i = 0; i < channels.length; i++) {
         if (channels[i].parent_id === null) {
             sortedChannels.push({ id: channels[i].id, title: channels[i].name, subChannels: [] });
+            // Remove channel from array ()
+            channels.splice(i, 1);
+            i--;
             for (j = 0; j < channels.length; j++) {
                 if (sortedChannels[m].id === channels[j].parent_id) {
                     sortedChannels[m].subChannels.push(channels[j]);
+                    // Remove channel from array ()
+                    channels.splice(j, 1);
+                    j--;
                 }
             }
             m++;
