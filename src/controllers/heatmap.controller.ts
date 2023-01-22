30243d4449f0a9f149a7c265389081f0dd9fae07
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-01-22 19:23:27 +0400

    change mongoose config

--- src/controllers/heatmap.controller.ts
+++ src/controllers/heatmap.controller.ts
@@ -4,12 +4,13 @@ import { IAuthRequest } from '../interfaces/request.interface';
 import { catchAsync, ApiError } from "../utils";
 import { databaseService, Guild } from 'tc-dbcomm'
 import httpStatus from 'http-status';
+import config from 'src/config';
 
 const getHeatmaps = catchAsync(async function (req: IAuthRequest, res: Response) {
     if (!await Guild.findOne({ guildId: req.params.guildId, user: req.user.discordId })) {
         throw new ApiError(httpStatus.NOT_FOUND, 'Guild not found');
     }
-    const connection = databaseService.connectionFactory(req.params.guildId, 'mongodb://127.0.0.1:27017');
+    const connection = databaseService.connectionFactory(req.params.guildId, config.mongoose.botURL);
     const heatmaps = await heatmapService.getHeatmaps(connection, req.body.startDate);
     res.send(heatmaps);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import config from './config';
 mongoose.set("strictQuery", false);
 
 // Connect to MongoDB
-mongoose.connect(config.mongoose.url)
+mongoose.connect(config.mongoose.serverURL)
     .then(() => {
         console.log('Connected to MongoDB!');
         // Run server
