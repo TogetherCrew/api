fe8e3b3723d8ea179b9bb9e490f18dfee2cd2cda
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-02-25 21:47:26 +0400

    [TEST]: update tests for notions/databases/:databaseId api

--- __tests__/integration/notion.test.ts
+++ __tests__/integration/notion.test.ts
@@ -6,32 +6,37 @@ import config from '../../src/config'
 setupTestDB();
 
 describe('Notion routes', () => {
-    describe('GET /api/v1/notion/databases/:databaseId', () => {
+    beforeEach(() => {
+        jest.resetModules()
+    });
+
+    describe('GET /api/v1/notion/databases', () => {
         test('should return 200 and database object if req data is ok', async () => {
             await request(app)
-                .get(`/api/v1/notions/databases/${config.notion.databaseId}`)
+                .get(`/api/v1/notion/databases`)
                 .send()
                 .expect(httpStatus.OK);
         })
         test('should return 400 if databaseId is not valid', async () => {
+            config.notion.databaseId = 'A';
             await request(app)
-                .get(`/api/v1/notions/databases/databaseId`)
+                .get(`/api/v1/notion/databases`)
                 .send()
                 .expect(httpStatus.BAD_REQUEST);
         })
         test('should return 404 if database not found', async () => {
+            config.notion.databaseId = '958a3d77051f4f7b848319819d776708'
             await request(app)
-                .get(`/api/v1/notions/databases/958a3d77051f4f7b848319819d776707`)
+                .get(`/api/v1/notion/databases`)
                 .send()
                 .expect(httpStatus.NOT_FOUND);
         })
         test('should return 401 if notion access token is missing', async () => {
             config.notion.apiKey = 'invalid'
             await request(app)
-                .get(`/api/v1/notions/databases/databaseId`)
+                .get(`/api/v1/notion/databases`)
                 .send()
-                .expect(httpStatus.BAD_REQUEST);
+                .expect(httpStatus.UNAUTHORIZED);
         })
     })
-
 });
