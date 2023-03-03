9fa54684bb3dfe4b1d9fba8b881b12f3a97c96df
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-03 21:19:26 +0400

    [REFACTOR]: refactor heatmap api

--- src/services/heatmap.service.ts
+++ src/services/heatmap.service.ts
@@ -8,59 +8,66 @@ import { Connection } from 'mongoose';
  * @returns {Array<Array<number>>}
  */
 async function getHeatmaps(connection: Connection, startDate: Date, endDate: Date) {
-    const heatmaps = await connection.models.HeatMap.aggregate([
+    try {
+        const heatmaps = await connection.models.HeatMap.aggregate([
 
 
-        // Stage1 : convert date from string to date type and extract needed data
-        {
-            $project: {
-                _id: 0,
-                date: { $convert: { input: "$date", to: "date", } },
-                lone_messages: 1,
-                thr_messages: 1,
-                replier: 1,
+            // Stage1 : convert date from string to date type and extract needed data
+            {
+                $project: {
+                    _id: 0,
+                    date: { $convert: { input: "$date", to: "date", } },
+                    lone_messages: 1,
+                    thr_messages: 1,
+                    replier: 1,
 
-            }
-        },
+                }
+            },
 
-        // Stage2 : find heatmaps between startDate and endDate
-        {
-            $match: {
-                'date': {
-                    $gte: new Date(startDate),
-                    $lte: new Date(endDate)
+            // Stage2 : find heatmaps between startDate and endDate
+            {
+                $match: {
+                    'date': {
+                        $gte: new Date(startDate),
+                        $lte: new Date(endDate)
+                    }
+                }
+            },
+
+            // Stage3 : provide one document for each element of interactions array
+            {
+                $unwind: {
+                    path: '$thr_messages',
+                    includeArrayIndex: "arrayIndex",
+                }
+            },
+
+            // Stage4 : extract needed data
+            {
+                $project: {
+                    'dayOfWeek': { $add: [{ $dayOfWeek: "$date" }, -1] },
+                    'hour': { $add: ['$arrayIndex', 1] },
+                    'interactions': { $add: ['$thr_messages', { $arrayElemAt: ['$lone_messages', '$arrayIndex'] }, { $arrayElemAt: ['$replier', '$arrayIndex'] }] },
+                }
+            },
+
+            // Stage5 : group documents base on day and hour
+            {
+                $group: {
+                    '_id': { dayOfWeek: '$dayOfWeek', hour: '$hour' }
+                    , interactions: { $sum: "$interactions" }
                 }
             }
-        },
+        ]);
 
-        // Stage3 : provide one document for each element of interactions array
-        {
-            $unwind: {
-                path: '$thr_messages',
-                includeArrayIndex: "arrayIndex",
-            }
-        },
+        // Convert Arrays of objects to array of 2D arrays
+        return heatmaps.map(object => [object._id.dayOfWeek, object._id.hour, object.interactions]);
 
-        // Stage4 : extract needed data
-        {
-            $project: {
-                'dayOfWeek': { $add: [{ $dayOfWeek: "$date" }, -1] },
-                'hour': { $add: ['$arrayIndex', 1] },
-                'interactions': { $add: ['$thr_messages', { $arrayElemAt: ['$lone_messages', '$arrayIndex'] }] },
-            }
-        },
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
 
-        // Stage5 : group documents base on day and hour
-        {
-            $group: {
-                '_id': { dayOfWeek: '$dayOfWeek', hour: '$hour' }
-                , interactions: { $sum: "$interactions" }
-            }
-        }
-    ]);
-
-    // Convert Arrays of objects to array of 2D arrays
-    return heatmaps.map(object => [object._id.dayOfWeek, object._id.hour, object.interactions]);
 }
 
 export default {
