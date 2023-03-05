bf1800b30bf30d12e7b7797d22a9aae8474cec3b
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-05 22:21:54 +0400

    [TEST]: add tests heatmaps/heatmapId/line-chart api

--- __tests__/integration/heatmap.test.ts
+++ __tests__/integration/heatmap.test.ts
@@ -4,7 +4,7 @@ import app from '../../src/app';
 import setupTestDB from '../utils/setupTestDB';
 import { userOne, insertUsers } from '../fixtures/user.fixture';
 import { userOneAccessToken } from '../fixtures/token.fixture';
-import { heatmapOne, heatmapTwo } from '../fixtures/heatmap.fixture';
+import { heatmapOne, heatmapTwo, heatmapThree, heatmapFour } from '../fixtures/heatmap.fixture';
 import { guildOne, insertGuilds } from '../fixtures/guilds.fixture';
 import { heatmapService, databaseService } from 'tc-dbcomm';
 import config from '../../src/config';
@@ -14,12 +14,11 @@ setupTestDB();
 
 describe('Guild routes', () => {
     const connection = databaseService.connectionFactory(guildOne.guildId, config.mongoose.botURL);
-    beforeEach(async () => {
-        await connection.dropDatabase();
-    });
     describe('POST /api/v1/heatmaps/:guildId/heatmap-chart', () => {
-
-        test('should return 200 and array of 2D arrays if req data is ok', async () => {
+        beforeEach(async () => {
+            await connection.dropDatabase();
+        });
+        test('should return 200 and heatmap chart data if req data is ok', async () => {
             await insertUsers([userOne]);
             await insertGuilds([guildOne]);
 
@@ -53,6 +52,63 @@ describe('Guild routes', () => {
                 .post(`/api/v1/heatmaps/${guildOne.guildId}/heatmap-chart`)
                 .set('Authorization', `Bearer ${userOneAccessToken}`)
                 .send({ startDate: new Date(), endDate: new Date(), timeZone: "Asia/Tehran" })
+                .expect(httpStatus.NOT_FOUND);
+        })
+    })
+
+    describe('POST /api/v1/heatmaps/:guildId/line-graph', () => {
+        beforeEach(async () => {
+            await connection.dropDatabase();
+        });
+        test('should return 200 and line graph data if req data is ok', async () => {
+            await insertUsers([userOne]);
+            await insertGuilds([guildOne]);
+
+            await heatmapService.createHeatMaps(connection, [heatmapOne, heatmapTwo]);
+            const res = await request(app)
+                .post(`/api/v1/heatmaps/${guildOne.guildId}/line-graph`)
+                .set('Authorization', `Bearer ${userOneAccessToken}`)
+                .send({ startDate: new Date("2023-01-21"), endDate: new Date("2023-01-24") })
+                .expect(httpStatus.OK);
+
+            expect(res.body.messages).toBe(18);
+            expect(res.body.emojis).toBe(9);
+            expect(res.body.msgPercentageChange).toBe(0);
+            expect(res.body.emojiPercentageChange).toBe(0);
+
+        })
+
+        test('should return 200 and line graph data (testing percentage change) if req data is ok', async () => {
+            await insertUsers([userOne]);
+            await insertGuilds([guildOne]);
+
+            await heatmapService.createHeatMaps(connection, [heatmapOne, heatmapTwo, heatmapThree, heatmapFour]);
+            const res = await request(app)
+                .post(`/api/v1/heatmaps/${guildOne.guildId}/line-graph`)
+                .set('Authorization', `Bearer ${userOneAccessToken}`)
+                .send({ startDate: new Date("2023-01-21"), endDate: new Date("2023-01-24") })
+                .expect(httpStatus.OK);
+
+            expect(res.body.messages).toBe(18);
+            expect(res.body.emojis).toBe(9);
+            expect(res.body.msgPercentageChange).toBe(100);
+            expect(res.body.emojiPercentageChange).toBe(-50);
+
+        })
+
+        test('should return 401 if access token is missing', async () => {
+            await request(app)
+                .post(`/api/v1/heatmaps/${guildOne.guildId}/line-graph`)
+                .send({ startDate: new Date(), endDate: new Date() })
+                .expect(httpStatus.UNAUTHORIZED);
+        })
+
+        test('should return 404 if guild not found', async () => {
+            await insertUsers([userOne]);
+            await request(app)
+                .post(`/api/v1/heatmaps/${guildOne.guildId}/line-graph`)
+                .set('Authorization', `Bearer ${userOneAccessToken}`)
+                .send({ startDate: new Date(), endDate: new Date() })
                 .expect(httpStatus.NOT_FOUND);
         })
     })
