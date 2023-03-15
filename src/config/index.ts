f7b6a488dbcb654acf5717e53f58b4b3ddf309a1
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-03-15 20:55:25 +0400

    [REFACTOR]: add locals

--- src/config/index.ts
+++ src/config/index.ts
@@ -38,6 +38,8 @@ export default {
     mongoose: {
         serverURL: `mongodb://${envVars.DB_USER}:${envVars.DB_PASSWORD}@${envVars.DB_HOST}:${envVars.DB_PORT}/${envVars.DB_NAME}`,
         botURL: `mongodb://${envVars.DB_USER}:${envVars.DB_PASSWORD}@${envVars.DB_HOST}:${envVars.DB_PORT}`,
+        // serverURL: `mongodb://127.0.0.1:27017/RnDAO-${envVars.NODE_ENV}`,
+        // botURL: "mongodb://127.0.0.1:27017"
     },
     discord: {
         clientId: envVars.DISCROD_CLIENT_ID,
