84903aa052ca81a5603f74477124465295578265
Mohammad Twin <scientists.twin@gmail.com>
2023-05-02 13:18:16 +0400

    feat: Sentry was added to the application

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import passport from "passport";
+import * as Sentry from "@sentry/node";
+import config from './config';
 import { jwtStrategy } from "./config/passport";
 import cors from "cors";
 import httpStatus from "http-status";
@@ -10,6 +12,12 @@ import { ApiError } from "./utils";
 import routes from "./routes/v1";
 
 const app: Application = express();
+
+// Initial Sentry
+Sentry.init({ dsn: config.sentry.dsn, tracesSampleRate: 1.0 });
+
+// Sentry Logger - The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
 // set security HTTP headers
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -37,6 +45,9 @@ app.use('/api/v1', routes);
 app.use((req, res, next) => {
     next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
+
+// Sentry Logger - The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
 
 app.use(error.errorConverter);
 app.use(error.errorHandler);
