16d0469ff23e59cb5e38d4f338781d132cc61c50
Mohammad Twin <scientists.twin@gmail.com>
2023-05-02 14:02:58 +0400

    feat: tracing was also added to the sentry

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,22 @@ import routes from "./routes/v1";
 const app: Application = express();
 
 // Initial Sentry
-Sentry.init({ dsn: config.sentry.dsn, tracesSampleRate: 1.0 });
+Sentry.init({ dsn: config.sentry.dsn, tracesSampleRate: 1.0, 
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Sentry.Integrations.Express({ app }),
+        // Automatically instrument Node.js libraries and frameworks
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ] 
+});
 
 // Sentry Logger - The request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
+
+// Sentry Tracer - TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler() as express.RequestHandler);
 
 // set security HTTP headers
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
