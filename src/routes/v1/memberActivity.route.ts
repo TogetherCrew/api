da690b48ad3096be05a528fae3a0b79c6c7d9cb1
Mohammad Twin <scientists.twin@gmail.com>
2023-06-21 13:02:54 +0400

    fix: memeber was changed to members in network graph endpoint

--- src/routes/v1/memberActivity.route.ts
+++ src/routes/v1/memberActivity.route.ts
@@ -10,7 +10,7 @@ router.post('/:guildId/active-members-composition-line-graph', auth(), validate(
 router.post('/:guildId/active-members-onboarding-line-graph', auth(), validate(memberActivityValidation.activeMembersOnboardingLineGraph), memberActivityController.activeMembersOnboardingLineGraph);
 router.post('/:guildId/disengaged-members-composition-line-graph', auth(), validate(memberActivityValidation.disengagedMembersCompositionLineGraph), memberActivityController.disengagedMembersCompositionLineGraph);
 router.post('/:guildId/inactive-members-line-graph', auth(), validate(memberActivityValidation.inactiveMembersLineGraph), memberActivityController.inactiveMembersLineGraph);
-router.get('/:guildId/member-interactions-graph', auth(), validate(memberActivityValidation.memberInteractionsGraph), memberActivityController.memberInteractionsGraph)
+router.get('/:guildId/members-interactions-graph', auth(), validate(memberActivityValidation.memberInteractionsGraph), memberActivityController.memberInteractionsGraph)
 
 export default router;
 
