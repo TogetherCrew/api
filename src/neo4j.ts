1eb4ad358f4c664d07e461a28ed906d4f695ffe7
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-08-29 14:11:17 +0400

    [FEATURE]: add logger for neo4j

--- src/neo4j.ts
+++ src/neo4j.ts
@@ -1,15 +1,26 @@
-import * as neo4j from 'neo4j-driver';
+import neo4j, { Driver } from 'neo4j-driver';
 import config from './config';
 import { Query } from 'neo4j-driver-core/types/types';
+import parentLogger from './config/logger';
+
+const logger = parentLogger.child({ module: 'Neo4j' });
 
 
-const driver = neo4j.driver(config.neo4j.url,
-                            neo4j.auth.basic(config.neo4j.user, config.neo4j.password),
-                            // https://github.com/neo4j/neo4j-javascript-driver#enabling-native-numbers
-                            { disableLosslessIntegers: true }
-                            )
+let driver: Driver;
+try {
+    driver = neo4j.driver(
+        config.neo4j.url,
+        neo4j.auth.basic(config.neo4j.user, config.neo4j.password),
+        { disableLosslessIntegers: true }
+    );
+    logger.info({ url: config.neo4j.url, user: config.neo4j.user, password: config.neo4j.password }, `Connected to Neo4j!`);
+} catch (error) {
+    logger.fatal({ url: config.neo4j.url, user: config.neo4j.user, password: config.neo4j.password }, `Failed to connect to Neo4j!`);
+}
 
-export async function read(cypher: Query, params = {}, database = config.neo4j.database){
+
+
+export async function read(cypher: Query, params = {}, database = config.neo4j.database) {
     const session = driver.session({
         defaultAccessMode: neo4j.session.READ,
         database,
@@ -17,16 +28,17 @@ export async function read(cypher: Query, params = {}, database = config.neo4j.d
 
     return session.run(cypher, params)
         .then(res => {
-            session.close()
+            session.close();
             return res
         })
         .catch(e => {
-            session.close()
+            logger.error({ cypher, params, e }, `Failed to run read operator!`);
+            session.close();
             throw e
         })
 }
 
-export async function write(cypher: Query, params = {}, database = config.neo4j.database){
+export async function write(cypher: Query, params = {}, database = config.neo4j.database) {
     const session = driver.session({
         defaultAccessMode: neo4j.session.WRITE,
         database,
@@ -37,6 +49,7 @@ export async function write(cypher: Query, params = {}, database = config.neo4j.
         session.close();
         return res;
     } catch (e) {
+        logger.error({ cypher, params, e }, `Failed to run write operator!`);
         session.close();
         throw e;
     }
