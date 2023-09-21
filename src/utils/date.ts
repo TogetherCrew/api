dd14a0dac27d49ce06031b857844f5ce5100f9da
Mohammad Twin <scientists.twin@gmail.com>
2023-09-21 12:15:39 +0400

    feat: a useful function was added for managing times

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -53,10 +53,25 @@ function getYesterdayUTCtimestamp(){
     return yesterdayUTCtimestamp
 }
 
+function get7daysAgoUTCtimestamp(){
+    const sevenDaysAgo = new Date()
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7)
+    
+    const year = sevenDaysAgo.getUTCFullYear()
+    const month = sevenDaysAgo.getUTCMonth()
+    const day = sevenDaysAgo.getUTCDate()
+
+    const sevenDaysAgoUTC = new Date(Date.UTC(year, month, day))
+    const sevenDaysAgoUTCtimestamp = sevenDaysAgoUTC.getTime()
+
+    return sevenDaysAgoUTCtimestamp
+}
+
 export default {
     shiftHeatmapsHours,
     calculateAdjustedDate,
-    getYesterdayUTCtimestamp
+    getYesterdayUTCtimestamp,
+    get7daysAgoUTCtimestamp
 }
 
 
