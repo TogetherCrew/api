21f86b903cf8965aa4df0d6561094e722cc23a70
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-10-10 16:48:18 +0330

    [FIXBUG]

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,7 +6,7 @@ import { permissions } from '../config/dicord';
 
 const getGuildsWithAdminRole = catchAsync(async function (req: IAuthRequest, res: Response) {
     const user = req.user;
-    const { access } = await tokenService.getDiscordAuth(user.discordId);
+    const { access } = await tokenService.getDiscordOAuth2Tokens(user.id);
     const guilds = await userService.getCurrentUserGuilds(access.token);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const guildsWithAdminRole = guilds.filter((guild: any) => (guild.permissions & permissions.manageServer) === permissions.manageServer);
--- src/interfaces/request.interface.ts
+++ src/interfaces/request.interface.ts
@@ -1,9 +1,10 @@
 import { IUser } from "@togethercrew.dev/db";
+import { HydratedDocument } from 'mongoose';
 import { Request } from "express";
 import { Session, SessionData } from 'express-session';
 
 export interface IAuthRequest extends Request {
-    user: IUser
+    user: HydratedDocument<IUser>
 }
 export interface ISessionRequest extends Request {
     session: Session & Partial<SessionData> & { [key: string]: any };
@@ -11,7 +12,7 @@ export interface ISessionRequest extends Request {
 
 export interface IAuthAndSessionRequest extends Request {
     session: Session & Partial<SessionData> & { [key: string]: any };
-    user: IUser
+    user: HydratedDocument<IUser>
 }
 
 export interface IHeatmapChartRequestBody extends Request {
