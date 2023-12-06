09ae985d72a261a62be1350afb1548a68e4bda00
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-12-06 16:56:26 +0330

    [FIXBUG]: fix bug related to create platform

--- src/controllers/platform.controller.ts
+++ src/controllers/platform.controller.ts
@@ -15,12 +15,13 @@ const createPlatform = catchAsync(async function (req: IAuthRequest, res: Respon
         throw new ApiError(httpStatus.NOT_FOUND, 'Community not found');
     }
 
-    let platformDoc = await platformService.getPlatformByFilter({ community: { $in: req.user.communities }, disconnectedAt: null, name: req.body.name });
+    // let platformDoc = await platformService.getPlatformByFilter({ community: { $in: req.user.communities }, disconnectedAt: null, name: req.body.name });
+    let platformDoc = await platformService.getPlatformByFilter({ community: req.body.community, disconnectedAt: null, name: req.body.name });
     if (platformDoc) {
         throw new ApiError(httpStatus.BAD_REQUEST, `Only can connect one ${req.body.name} platform`);
     }
 
-    platformDoc = await platformService.getPlatformByFilter({ community: { $in: req.user.communities }, 'metadata.id': req.body.metadata.id, disconnectedAt: { $ne: null } });
+    platformDoc = await platformService.getPlatformByFilter({ community: req.body.community, 'metadata.id': req.body.metadata.id, disconnectedAt: { $ne: null } });
     if (platformDoc) {
         const platform = await platformService.updatePlatform(platformDoc, { disconnectedAt: null });
         return res.status(httpStatus.CREATED).send(platform);
