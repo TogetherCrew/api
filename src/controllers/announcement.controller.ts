715bfcd947e4b4fd0fbdf49d797dd8dbb6355462
Mohammad Twin <scientists.twin@gmail.com>
2023-12-27 09:48:47 +0400

    feat: check announcement ownership in announcement controller

--- src/controllers/announcement.controller.ts
+++ src/controllers/announcement.controller.ts
@@ -1,8 +1,15 @@
 import { Response } from 'express';
 import { IAuthRequest } from '../interfaces/Request.interface';
-import { catchAsync } from "../utils";
+import { ApiError, catchAsync } from "../utils";
+import { communityService } from '../services';
+import httpStatus from 'http-status';
 
 const createAnnouncement = catchAsync(async function (req: IAuthRequest, res: Response) {
+    const community = await communityService.getCommunityByFilter({ _id: req.body.community, users: req.user.id });
+    if (!community) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Community not found');
+    }
+    
     console.log(req.body)
     res.send({ status: 'ok' })
 })
