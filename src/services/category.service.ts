ed888350a60857e6d0e2955e32fefe129639cea3
Mohammad Twin <scientists.twin@gmail.com>
2024-02-21 09:38:14 +0400

    feat: category routes were added
    feat: swagger of the category were added
    
    feat: all logic were controller

---
+++ src/services/category.service.ts
@@ -0,0 +1,25 @@
+
+const Categories = [
+    "all_joined",
+    "all_joined_day",
+    "all_consistent",
+    "all_vital",
+    "all_active",
+    "all_connected",
+    "all_paused",
+    "all_new_disengaged",
+    "all_disengaged",
+    "all_unpaused",
+    "all_returned",
+    "all_new_active",
+    "all_still_active",
+    "all_dropped"
+] as const;
+
+const getCategories = () => {
+    return Categories
+}
+
+export default {
+    getCategories
+}
