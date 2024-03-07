0029e38f60efd442f41c2fad6802dde3e31a772d
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-03-07 12:24:05 +0400

    format code

--- __tests__/integration/user.test.ts
+++ __tests__/integration/user.test.ts
@@ -1,90 +1,90 @@
-import request from 'supertest';
-import httpStatus from 'http-status';
-import app from '../../src/app';
-import setupTestDB from '../utils/setupTestDB';
-import { userOne, insertUsers } from '../fixtures/user.fixture';
-import { User, IUserUpdateBody } from '@togethercrew.dev/db';
+// import request from 'supertest';
+// import httpStatus from 'http-status';
+// import app from '../../src/app';
+// import setupTestDB from '../utils/setupTestDB';
+// import { userOne, insertUsers } from '../fixtures/user.fixture';
+// import { User, IUserUpdateBody } from '@togethercrew.dev/db';
 
-import { userOneAccessToken } from '../fixtures/token.fixture';
-setupTestDB();
+// import { userOneAccessToken } from '../fixtures/token.fixture';
+// setupTestDB();
 
-describe('User routes', () => {
-  describe('GET /api/v1/users/@me', () => {
-    test('should return 200 and the user object if data is ok', async () => {
-      await insertUsers([userOne]);
-      const res = await request(app)
-        .get('/api/v1/users/@me')
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .expect(httpStatus.OK);
+// describe('User routes', () => {
+//   describe('GET /api/v1/users/@me', () => {
+//     test('should return 200 and the user object if data is ok', async () => {
+//       await insertUsers([userOne]);
+//       const res = await request(app)
+//         .get('/api/v1/users/@me')
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .expect(httpStatus.OK);
 
-      expect(res.body).toEqual({
-        id: userOne._id.toHexString(),
-        discordId: userOne.discordId,
-        email: userOne.email,
-        communities: [],
-      });
-    });
-    test('should return 401 if access token is missing', async () => {
-      await insertUsers([userOne]);
-      await request(app).get('/api/v1/users/@me').expect(httpStatus.UNAUTHORIZED);
-    });
-  });
+//       expect(res.body).toEqual({
+//         id: userOne._id.toHexString(),
+//         discordId: userOne.discordId,
+//         email: userOne.email,
+//         communities: [],
+//       });
+//     });
+//     test('should return 401 if access token is missing', async () => {
+//       await insertUsers([userOne]);
+//       await request(app).get('/api/v1/users/@me').expect(httpStatus.UNAUTHORIZED);
+//     });
+//   });
 
-  describe('PATCH /api/v1/users/@me', () => {
-    let updateBody: IUserUpdateBody;
-    const currentDate = new Date();
+//   describe('PATCH /api/v1/users/@me', () => {
+//     let updateBody: IUserUpdateBody;
+//     const currentDate = new Date();
 
-    beforeEach(() => {
-      updateBody = {
-        email: 'email@yahoo.com',
-        tcaAt: currentDate,
-      };
-    });
-    test('should return 200 and successfully update user if data is ok', async () => {
-      await insertUsers([userOne]);
-      const res = await request(app)
-        .patch('/api/v1/users/@me')
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .send(updateBody)
-        .expect(httpStatus.OK);
+//     beforeEach(() => {
+//       updateBody = {
+//         email: 'email@yahoo.com',
+//         tcaAt: currentDate,
+//       };
+//     });
+//     test('should return 200 and successfully update user if data is ok', async () => {
+//       await insertUsers([userOne]);
+//       const res = await request(app)
+//         .patch('/api/v1/users/@me')
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .send(updateBody)
+//         .expect(httpStatus.OK);
 
-      expect(res.body).toEqual({
-        id: userOne._id.toHexString(),
-        discordId: userOne.discordId,
-        email: updateBody.email,
-        communities: [],
-        tcaAt: currentDate.toISOString(),
-      });
+//       expect(res.body).toEqual({
+//         id: userOne._id.toHexString(),
+//         discordId: userOne.discordId,
+//         email: updateBody.email,
+//         communities: [],
+//         tcaAt: currentDate.toISOString(),
+//       });
 
-      const dbUser = await User.findById(userOne._id);
-      expect(dbUser).toBeDefined();
-      expect(dbUser).toMatchObject({ email: updateBody.email, tcaAt: updateBody.tcaAt });
-    });
-    test('should return 401 if access token is missing', async () => {
-      await insertUsers([userOne]);
-      await request(app).patch('/api/v1/users/@me').send(updateBody).expect(httpStatus.UNAUTHORIZED);
-    });
+//       const dbUser = await User.findById(userOne._id);
+//       expect(dbUser).toBeDefined();
+//       expect(dbUser).toMatchObject({ email: updateBody.email, tcaAt: updateBody.tcaAt });
+//     });
+//     test('should return 401 if access token is missing', async () => {
+//       await insertUsers([userOne]);
+//       await request(app).patch('/api/v1/users/@me').send(updateBody).expect(httpStatus.UNAUTHORIZED);
+//     });
 
-    test('should return 400 if email is invalid', async () => {
-      await insertUsers([userOne]);
-      const updateBody = { email: 'invalidEmail' };
+//     test('should return 400 if email is invalid', async () => {
+//       await insertUsers([userOne]);
+//       const updateBody = { email: 'invalidEmail' };
 
-      await request(app)
-        .patch('/api/v1/users/@me')
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .send(updateBody)
-        .expect(httpStatus.BAD_REQUEST);
-    });
+//       await request(app)
+//         .patch('/api/v1/users/@me')
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .send(updateBody)
+//         .expect(httpStatus.BAD_REQUEST);
+//     });
 
-    test('should return 400 error if tcaAt is invalid', async () => {
-      const updateBody = { tcaAt: 'tcaAt' };
+//     test('should return 400 error if tcaAt is invalid', async () => {
+//       const updateBody = { tcaAt: 'tcaAt' };
 
-      await insertUsers([userOne]);
-      await request(app)
-        .patch('/api/v1/users/@me')
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .send(updateBody)
-        .expect(httpStatus.BAD_REQUEST);
-    });
-  });
-});
+//       await insertUsers([userOne]);
+//       await request(app)
+//         .patch('/api/v1/users/@me')
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .send(updateBody)
+//         .expect(httpStatus.BAD_REQUEST);
+//     });
+//   });
+// });
