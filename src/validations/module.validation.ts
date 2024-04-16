3863a7f0a7d6b8c237d6de4bd86e3810259d52c9
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-04-16 12:00:52 +0400

    refactor dynamicModuleUpdate

--- src/validations/module.validation.ts
+++ src/validations/module.validation.ts
@@ -1,4 +1,4 @@
-import Joi from 'joi';
+import Joi, { ObjectSchema } from 'joi';
 import { objectId } from './custom.validation';
 import { Types } from 'mongoose';
 import { Request } from 'express';
@@ -26,56 +26,117 @@ const getModule = {
   }),
 };
 
-const dynamicModuleUpdate = (req: Request) => {
-  const module = req.module;
-  if (module?.name === 'hivemind') {
-    return {
-      params: Joi.object().keys({
-        moduleId: Joi.required().custom(objectId),
-      }),
-      body: Joi.object()
-        .required()
-        .keys({
-          options: Joi.object()
-            .required()
-            .keys({
-              platforms: Joi.array().items(
-                Joi.object().keys({
-                  name: Joi.string().required().valid('discord'),
-                  platform: Joi.string().custom(objectId).required(),
-                  metadata: Joi.object()
-                    .required()
-                    .when('name', {
-                      switch: [
-                        {
-                          is: 'discord',
-                          then: Joi.object().keys({
-                            answering: Joi.object().keys({
-                              selectedChannels: Joi.array().items(Joi.string()).required(),
-                            }),
-                            learning: Joi.object().keys({
-                              selectedChannels: Joi.array().items(Joi.string()).required(),
-                              fromDate: Joi.date().required(),
-                            }),
-                          }),
-                        },
-                      ],
-                      otherwise: Joi.any().forbidden(),
-                    }),
-                }),
-              ),
-            }),
-        }),
-    };
-  } else {
-    req.allowInput = false;
-    return {};
-  }
-};
+// const dynamicModuleUpdate = (req: Request) => {
+//   const module = req.module;
+//   if (module?.name === 'hivemind') {
+//     return {
+//       params: Joi.object().keys({
+//         moduleId: Joi.required().custom(objectId),
+//       }),
+//       body: Joi.object()
+//         .required()
+//         .keys({
+//           options: Joi.object()
+//             .required()
+//             .keys({
+//               platforms: Joi.array().items(
+//                 Joi.object().keys({
+//                   name: Joi.string().required().valid('discord'),
+//                   platform: Joi.string().custom(objectId).required(),
+//                   metadata: Joi.object()
+//                     .required()
+//                     .when('name', {
+//                       switch: [
+//                         {
+//                           is: 'discord',
+//                           then: Joi.object().keys({
+//                             answering: Joi.object().keys({
+//                               selectedChannels: Joi.array().items(Joi.string()).required(),
+//                             }),
+//                             learning: Joi.object().keys({
+//                               selectedChannels: Joi.array().items(Joi.string()).required(),
+//                               fromDate: Joi.date().required(),
+//                             }),
+//                           }),
+//                         },
+//                       ],
+//                       otherwise: Joi.any().forbidden(),
+//                     }),
+//                 }),
+//               ),
+//             }),
+//         }),
+//     };
+//   } else {
+//     req.allowInput = false;
+//     return {};
+//   }
+// };
+
 const deleteModule = {
   params: Joi.object().keys({
     moduleId: Joi.string().custom(objectId).required(),
   }),
+};
+
+const hivemindDiscordMetadata = () => {
+  return Joi.object().keys({
+    answering: Joi.object().keys({
+      selectedChannels: Joi.array().items(Joi.string()).required(),
+    }),
+    learning: Joi.object().keys({
+      selectedChannels: Joi.array().items(Joi.string()).required(),
+      fromDate: Joi.date().required(),
+    }),
+  });
+};
+
+const hivemindOptions = () => {
+  return Joi.object().keys({
+    platforms: Joi.array().items(
+      Joi.object().keys({
+        name: Joi.string().required().valid('discord'),
+        platform: Joi.string().custom(objectId).required(),
+        metadata: Joi.when('name', {
+          switch: [
+            {
+              is: 'discord',
+              then: hivemindDiscordMetadata(),
+            },
+          ],
+          otherwise: Joi.any().forbidden(),
+        }).required(),
+      }),
+    ),
+  });
+};
+
+const dynamicModuleUpdate = (req: any) => {
+  const moduleName = req.module?.name;
+  const paramsOption = {
+    params: Joi.object().keys({
+      moduleId: Joi.string().custom(objectId).required(),
+    }),
+  };
+  let bodyOption = {};
+
+  switch (moduleName) {
+    case 'hivemind':
+      bodyOption = {
+        body: Joi.object().required().keys({
+          options: hivemindOptions(),
+        }),
+      };
+      break;
+    default:
+      req.allowInput = false;
+      return {};
+  }
+
+  return {
+    ...paramsOption,
+    ...bodyOption,
+  };
 };
 
 export default {
