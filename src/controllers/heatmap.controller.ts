9fd1995c0ce636e7d1dfb125d83c402419055548
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-08-08 20:45:06 +0400

    debug: add logs

--- src/controllers/heatmap.controller.ts
+++ src/controllers/heatmap.controller.ts
@@ -10,7 +10,7 @@ const heatmapChart = catchAsync(async function (req: IAuthAndPlatform, res: Resp
     return res.send(charts.fillHeatmapChart([]));
   }
   const platformConnection = await DatabaseManager.getInstance().getPlatformDb(req.platform?.id);
-  console.log(await platformConnection.models.HeatMap.find({}));
+  console.log('SALAM', await platformConnection.models.HeatMap.find({}));
   let heatmaps = await heatmapService.getHeatmapChart(platformConnection, req.body);
   const timeZoneOffset = parseInt(moment().tz(req.body.timeZone).format('Z'));
 
@@ -23,7 +23,7 @@ const heatmapChart = catchAsync(async function (req: IAuthAndPlatform, res: Resp
 
 const lineGraph = catchAsync(async function (req: IAuthAndPlatform, res: Response) {
   const platformConnection = await DatabaseManager.getInstance().getPlatformDb(req.platform?.id);
-  console.log(await platformConnection.models.HeatMap.find({}));
+  console.log('SALAM', await platformConnection.models.HeatMap.find({}));
   let lineGraph = await heatmapService.lineGraph(platformConnection, req.body.startDate, req.body.endDate);
   lineGraph = charts.fillHeatmapLineGraph(lineGraph, req.body.startDate, req.body.endDate);
   res.send(lineGraph);
