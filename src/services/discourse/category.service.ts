14c6e5bcd328d1c27a98ab44677016f9978d21d3
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-09-12 17:15:51 +0400

    feat: add pagination for category property api

--- src/services/discourse/category.service.ts
+++ src/services/discourse/category.service.ts
@@ -17,18 +17,34 @@ async function getCategoriesByEndPoint(filters: any, options: any): Promise<any>
   limit = limit && parseInt(limit, 10) > 0 ? parseInt(limit, 10) : 10;
   page = page && parseInt(page, 10) > 0 ? parseInt(page, 10) : 1;
   skip = (page - 1) * limit;
+  const totalQuery = `
+    MATCH (forum:DiscourseForum {endpoint: "${endPoint}"})
+    MATCH (dc:DiscourseCategory {forumUuid: forum.uuid})
+    ${name ? `WHERE dc.name =~ '(?i).*${name}.*'` : ''}
+    RETURN COUNT(dc) as totalResults
+  `;
   const query = `
-  MATCH (forum:DiscourseForum {endpoint: "${endPoint}"})
-  MATCH (dc:DiscourseCategory {forumUuid: forum.uuid})
-  ${name ? `WHERE dc.name =~ '(?i).*${name}.*'` : ''}
-  RETURN dc.id as categoryId, dc.name as name, dc.color as color, dc.descriptionText as description
-  ORDER BY ${sortBy ? sortBy : 'dc.id'}
-  SKIP ${skip}
-  LIMIT ${limit}
-`;
+    MATCH (forum:DiscourseForum {endpoint: "${endPoint}"})
+    MATCH (dc:DiscourseCategory {forumUuid: forum.uuid})
+    ${name ? `WHERE dc.name =~ '(?i).*${name}.*'` : ''}
+    RETURN dc.id as categoryId, dc.name as name, dc.color as color, dc.descriptionText as description
+    ORDER BY ${sortBy ? sortBy : 'dc.id'}
+    SKIP ${skip}
+    LIMIT ${limit}
+  `;
+
   try {
+    const totalResultDataNeo4j = await Neo4j.read(totalQuery);
+    const { records: totalResultData } = totalResultDataNeo4j;
+    // @ts-ignore
+    const { _fieldLookup, _fields } = totalResultData[0];
+    const totalResults = _fields[_fieldLookup['totalResults']] as number;
+
+    // Fetch paginated category results
     const neo4jCategories = await Neo4j.read(query);
     const { records: categories } = neo4jCategories;
+
+    // Map the results to an object array
     const CategoryList = categories.map((category) => {
       // @ts-ignore
       const { _fieldLookup, _fields } = category;
@@ -38,7 +54,18 @@ async function getCategoriesByEndPoint(filters: any, options: any): Promise<any>
       const description = _fields[_fieldLookup['description']] as string;
       return { categoryId, name, color, description };
     });
-    return CategoryList;
+
+    // Calculate total pages
+    const totalPages = Math.ceil(totalResults / limit);
+
+    // Return structured response
+    return {
+      results: CategoryList,
+      page,
+      limit,
+      totalPages,
+      totalResults,
+    };
   } catch (error) {
     logger.error(error, 'Failed to get category list by endpoint');
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Failed to get category list by endpoint');
