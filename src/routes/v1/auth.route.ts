b355dcf04248c85410416aa42960a15d9a14d2f7
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-01-21 07:42:39 +0100

    chore: add telegram handle callback route

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+
 import { authController } from '../../controllers';
+import { auth, validate } from '../../middlewares';
 import { authValidation } from '../../validations';
-import { validate, auth } from '../../middlewares';
+
 const router = express.Router();
 
 // Routes
 router.get('/discord/authorize', authController.discordAuthorize);
 router.get('/discord/authorize/callback', authController.discordAuthorizeCallback);
+router.get('/telegram/authorize/callback', authController.discordAuthorizeCallback);
+
 router.post('/generate-token', auth(), validate(authValidation.generateToken), authController.generateToken);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
