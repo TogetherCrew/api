200807c4b23c6b41a83f5d2172ff639e4d4e7313
Cyrille <1618640+cyri113@users.noreply.github.com>
2025-03-26 14:39:46 +0100

    Update hivemind.controller.ts

--- src/controllers/hivemind.controller.ts
+++ src/controllers/hivemind.controller.ts
@@ -9,8 +9,8 @@ const askQuestion = catchAsync(async function (req: IAuthRequest, res: Response)
   console.debug('Body', req.body.communityId, req.body.question);
   req.setTimeout(6 * 60 * 1000);
   res.setTimeout(6 * 60 * 1000);
-  const awnser = await HivemindTemporalService.triggerWorkflow(req.body.communityId, req.body.question, false);
-  res.status(httpStatus.OK).send({ awnser });
+  const answer = await HivemindTemporalService.triggerWorkflow(req.body.communityId, req.body.question, false);
+  res.status(httpStatus.OK).send({ answer });
 });
 
 export default {
